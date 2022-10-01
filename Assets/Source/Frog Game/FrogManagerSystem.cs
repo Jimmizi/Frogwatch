96414a83f18d8f9f58923c7942090f7c95bb0afd
Mizicoca <50930532+Mizicoca@users.noreply.github.com>
2022-10-01 11:33:29 +0100

    Frog files

---
+++ Assets/Source/Frog Game/FrogManagerSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogManagerSystem : SystemObjectWithVars<FrogSystemVars>
+{
+    
+
+    public override void AwakeService()
+    {
+        
+    }
+
+    public override void StartService()
+    {
+        
+    }
+
+    public override void UpdateService()
+    {
+        
+    }
+
+    public override void FixedUpdateService()
+    {
+        
+    }
+}
---
+++ Assets/Source/Frog Game/FrogSystemVars.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogSystemVars : ServiceVars
+{
+    public List<BoxCollider2D> FrogMovementBounds = new ();
+}
